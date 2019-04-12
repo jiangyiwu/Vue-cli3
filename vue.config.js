02ae62a5ae465cdfed6ed53553fe28b7a2df945c
jiangyiwu <jiangyiwu@polyv.net>
2019-04-12 17:09:28 +0800

    2019年4月12日17:09:26

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.NOOD_ENV === 'production' ? '/helloWorld/' : '/',
   chainWebpack: config => {
-    console.log(config.module, '0-0-0-');
+    // console.log(config.module, '0-0-0-');
   },
   devServer: {
     port: 2333,
